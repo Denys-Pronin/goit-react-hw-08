a96cb22fc482856e267833924f2d54e9a89cd719
Denys Pronin <denispronindev@gmail.com>
2024-08-17 13:04:25 +0300

    upd AppBar

--- src/components/AppBar/AppBar.jsx
+++ src/components/AppBar/AppBar.jsx
@@ -1,9 +1,6 @@
-import { Link, NavLink } from "react-router-dom";
 import s from "./AppBar.module.css";
-import clsx from "clsx";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { selectIsLoggedIn } from "../../redux/auth/selectors";
-import { logout } from "../../redux/auth/operations";
 import UserMenu from "../UserMenu/UserMenu";
 import AuthNav from "../AuthNav/AuthNav";
 import Navigation from "../Navigation/Navigation";
